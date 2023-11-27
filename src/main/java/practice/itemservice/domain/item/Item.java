545e399263907252bf7d3d0dec85c95f459a06e2
シム ウク <astralkaiser89@gmail.com>
2023-11-27 13:46:04 +0900

    初期設定

--- src/main/java/practice/itemservice/domain/item/Item.java
+++ src/main/java/practice/itemservice/domain/item/Item.java
@@ -1,2 +1,20 @@
-package practice.itemservice.domain.item;public class Item {
+package practice.itemservice.domain.item;
+
+import lombok.Data;
+
+@Data
+public class Item {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
 }
